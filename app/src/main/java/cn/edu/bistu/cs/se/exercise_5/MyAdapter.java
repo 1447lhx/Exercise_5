813e0cb5e438056072ffb2e519ca732781f4389b
1447lhx <1959142791@qq.com>
2016-09-11 14:07:56 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/bistu/cs/se/exercise_5/MyAdapter.java
@@ -0,0 +1,38 @@
+package cn.edu.bistu.cs.se.exercise_5;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+ import android.view.View;
+ import android.view.ViewGroup;
+ import android.widget.TextView;
+ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+      private String[] mDataset;
+       public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                // create a new view
+                 View v = LayoutInflater.from(parent.getContext())
+                       .inflate(R.layout.myview, parent, false);
+               // set the view's size, margins, paddings and layout parameters
+                ViewHolder vh = new ViewHolder(v);
+                 return vh;
+           }
+             public MyAdapter(String[] myDataset) {
+               mDataset = myDataset;
+           }
+
+       public void onBindViewHolder(MyAdapter.ViewHolder holder, int position) {
+                holder.mTextView.setText(mDataset[position]);
+            }
+
+       public int getItemCount() {
+                return mDataset.length;
+          }
+              public class ViewHolder extends RecyclerView.ViewHolder {
+              public TextView mTextView;
+              public ViewHolder(View itemView) {
+                      super(itemView);
+                     mTextView =(TextView) itemView.findViewById(R.id.textView);
+
+                   }
+           }
+  }
+
